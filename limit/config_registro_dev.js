3a45cb7f0331a9901be2e982bef4f7fe87c1d965
Muertoparado <nicolas.casanova20@gmail.com>
2023-08-09 21:55:33 -0500

    feat: :rocket: post registro devolucion pero todavia genera error

--- limit/config_registro_dev.js
+++ limit/config_registro_dev.js
@@ -1,12 +1,12 @@
 import rateLimit from 'express-rate-limit';
-export let limitAlquiler = () =>{
+export let limitDevolucion = () =>{
     return rateLimit({
         windowMs:30* 1000,
         max: 6,
         standardHeaders: true,
         legalcyHeaders: false,
         skip: (req,res)=>{
-            if(req.headers["content-length"]>170){
+            if(req.headers["content-length"]>250){
                 res.status(413).send({
                     status:413,
                     message: "el tamano es incorrecto"
