10624faa75820088a6ff0efdae0bd959bade89eb
Muertoparado <nicolas.casanova20@gmail.com>
2023-08-09 20:15:01 -0500

    feat: :sparkles: post aquiler

--- routes/PostTablas.js
+++ routes/PostTablas.js
@@ -146,6 +146,31 @@ appCampus.post('/cliente', limitCliente(), async (req, res) => {
    
 });
 
+appCampus.post('/alquiler', limitCliente(), async (req, res) => {
+   if (!req.body) {
+      return res.status(400).json({ message: 'Invalid request body' });
+   }
+   try {
+      const client = await con(); // Obtén la conexión a la base de datos
+     // const db = await client.db(); // Obtiene la instancia de la base de datos
 
+      const collection = client.collection("alquiler");
+      const myobj = req.body;
+      const result = await collection.insertOne(myobj);
+      console.log("Documento insertado");
+      console.log(req.rateLimit);;
+      return res.json({
+         status: "success",
+         message: "Documento insertado",
+         document: result.ops 
+      });
+      
+      
+   } catch (error) {
+      console.log("Error al insertar documento:", error);
+      return res.status(500).json({ message: 'Error al insertar documento' });
+   }
+   
+});
 
 export default appCampus;
